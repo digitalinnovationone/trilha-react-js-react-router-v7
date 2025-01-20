535d63c3f047573c36188777bb180fe7c9b11bfa
Michele Ambrosio <micheleambrosio.dev@gmail.com>
2025-01-20 08:55:34 -0300

    install react router and configure simple routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import App from "./App.tsx";
+import CouponsPage from "./pages/Coupons/index.tsx";
+import DashboardPage from "./pages/Dashboard/index.tsx";
+import MessagesPage from "./pages/Messages/index.tsx";
+import NotificationsPage from "./pages/Notifications/index.tsx";
+import SettingsPage from "./pages/Settings/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="coupons" element={<CouponsPage />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="messages" element={<MessagesPage />} />
+        <Route path="notifications" element={<NotificationsPage />} />
+        <Route path="settings" element={<SettingsPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
+
